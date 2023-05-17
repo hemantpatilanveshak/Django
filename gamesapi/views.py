094fbb53465e7f42f270bb083e82b2f8453688c5
hemantpatilanveshak <hemant.patil@anveshak.com>
2023-05-17 14:09:14 +0530

    id changed to xy

--- gamesapi/views.py
+++ gamesapi/views.py
@@ -51,7 +51,7 @@ class GameDetails(APIView):
 
     def put(self,request,id):
         try:
-            obj = Games.objects.get(id=id)
+            obj = Games.objects.get(xy=id)
         except Games.DoesNotExist:
             return Response({"Message":"Game ID Does not exists"})
 
