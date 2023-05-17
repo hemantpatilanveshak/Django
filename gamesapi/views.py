48511986cf7e08fd02e4399e93125b19ed3550d4
hemantpatilanveshak <hemant.patil@anveshak.com>
2023-05-17 14:13:58 +0530

    chnaged er

--- gamesapi/views.py
+++ gamesapi/views.py
@@ -51,7 +51,7 @@ class GameDetails(APIView):
 
     def put(self,request,id):
         try:
-            obj = Games.objects.get(pk=id)
+            obj = Games.objects.get(er=id)
         except Games.DoesNotExist:
             return Response({"Message":"Game ID Does not exists"})
 
